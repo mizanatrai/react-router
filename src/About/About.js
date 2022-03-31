8e9e1aa0182d68d7dd04c62deca7c8745a18dab0
Mizanur Rahman <mizanbd.eee@gmail.com>
2022-03-31 22:53:26 +0600

    Components added

---
+++ src/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>What do you want to know about us?</h2>
+            <p><small>We are all human being. That's our only identity!</small></p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Friends/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h2>This is friends zone</h2>
+        </div>
+    );
+};
+
+export default Friends;
